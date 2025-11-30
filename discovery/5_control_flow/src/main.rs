12ce2e27b41488a7787d4e580ee14144428092dd
LucasGuerraS <lucasgasardo@gmail.com>
2025-11-30 19:35:02 -0300

    refact: change project structure for more clarity

---
+++ discovery/5_control_flow/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let evaluation: bool = true;
+
+    match evaluation {
+        true => {
+            println!("The value is true");
+        }
+        false => {
+            println!("The value is false");
+        }
+    }
+}
