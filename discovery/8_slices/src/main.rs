01acb299ac77cecf0eb7af027e1074a9b051d474
LucasGuerraS <lucasgasardo@gmail.com>
2025-12-02 11:29:12 -0300

    feat: add som examples of slices

--- discovery/8_slices/src/main.rs
+++ discovery/8_slices/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    // String slice
+    let language = String::from("Rustacian");
+    let slice = &language[0..4];
+    println!("{slice}");
+
+    // array slice
+    let values = [10, 51, 18, 9];
+    let my_slice = &values[0..2];
+    println!("{my_slice:?}");
 }
